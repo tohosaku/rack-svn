92ab6f4372c03c77ee42ef5e7527850dd435418e
Takashi Kato <ny@cosmichorror.org>
2012-07-29 20:19:03 +0900

    Fix indent only

--- lib/rack/svn/directory.rb
+++ lib/rack/svn/directory.rb
@@ -14,151 +14,151 @@ module Rack
   # If +app+ is not specified, a Rack::File of the same +root+ will be used.
 
   module Svn
-  class Directory
-    DIR_FILE = "<tr><td class='name'><a href='%s'>%s</a></td><td class='size'>%s</td><td class='type'>%s</td><td class='mtime'>%s</td></tr>"
-    DIR_PAGE = <<-PAGE
-<html><head>
-  <title>%s</title>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <style type='text/css'>
-table { width:100%%; }
-.name { text-align:left; }
-.size, .mtime { text-align:right; }
-.type { width:11em; }
-.mtime { width:15em; }
-  </style>
-</head><body>
-<h1>%s</h1>
-<hr />
-<table>
-  <tr>
-    <th class='name'>Name</th>
-    <th class='size'>Size</th>
-    <th class='type'>Type</th>
-    <th class='mtime'>Last Modified</th>
-  </tr>
-%s
-</table>
-<hr />
-</body></html>
-    PAGE
+    class Directory
+      DIR_FILE = "<tr><td class='name'><a href='%s'>%s</a></td><td class='size'>%s</td><td class='type'>%s</td><td class='mtime'>%s</td></tr>"
+      DIR_PAGE = <<-PAGE
+      <html><head>
+      <title>%s</title>
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+      <style type='text/css'>
+      table { width:100%%; }
+      .name { text-align:left; }
+      .size, .mtime { text-align:right; }
+      .type { width:11em; }
+      .mtime { width:15em; }
+      </style>
+      </head><body>
+      <h1>%s</h1>
+      <hr />
+      <table>
+      <tr>
+      <th class='name'>Name</th>
+      <th class='size'>Size</th>
+      <th class='type'>Type</th>
+      <th class='mtime'>Last Modified</th>
+      </tr>
+      %s
+      </table>
+      <hr />
+      </body></html>
+      PAGE
 
-    attr_reader :files
-    attr_accessor :root, :path
+      attr_reader :files
+      attr_accessor :root, :path
 
-    def initialize(root, app=nil)
-      @root = root
-      @app = app || Rack::Svn::File.new(@root)
-    end
-
-    def call(env)
-      dup._call(env)
-    end
-
-    F = ::File
-
-    def _call(env)
-      @env = env
-      @branch_name = env['PATH_PREFIX'] || ""
-      @script_name = env['SCRIPT_NAME']
-      @path_info = Utils.unescape(env['PATH_INFO'])
-
-      if forbidden = check_forbidden
-        forbidden
-      else
-        @path = F.join(@root, @branch_name, @path_info).sub(/\/$/,'')
-        list_path
+      def initialize(root, app=nil)
+        @root = root
+        @app = app || Rack::Svn::File.new(@root)
       end
-    end
 
-    def check_forbidden
-      return unless @path_info.include? ".."
-
-      body = "Forbidden\n"
-      size = Rack::Utils.bytesize(body)
-      return [403, {"Content-Type" => "text/plain",
-        "Content-Length" => size.to_s,
-        "X-Cascade" => "pass"}, [body]]
-    end
-
-    def list_directory
-      @files = [['../','Parent Directory','','','']]
-
-      context   = ::Svn::Client::Context.new
-
-      context.list(@path ,"HEAD") do |name,dirent,lock,abs_path|
-        basename = F.basename(name)
-        ext = F.extname(name)
-
-        url = F.join(@script_name, @path_info, basename)
-        size = dirent.size
-        type = dirent.directory? ? 'directory' : Mime.mime_type(ext)
-        size = dirent.directory? ? '-' : filesize_format(size)
-        mtime = dirent.time2.httpdate
-        url << '/'  if dirent.directory?
-        basename << '/'  if dirent.directory?
-
-        @files << [ url, basename, size, type, mtime ]
+      def call(env)
+        dup._call(env)
       end
-      return [ 200, {'Content-Type'=>'text/html; charset=utf-8'}, self ]
-    end
 
-    #def stat(node, max = 10)
-    #  F.stat(node)
-    #rescue Errno::ENOENT, Errno::ELOOP
-    #  return nil
-    #end
+      F = ::File
 
-    # TODO: add correct response if not readable, not sure if 404 is the best
-    #       option
-    def list_path
+      def _call(env)
+        @env = env
+        @branch_name = env['PATH_PREFIX'] || ""
+        @script_name = env['SCRIPT_NAME']
+        @path_info = Utils.unescape(env['PATH_INFO'])
 
-      context   = ::Svn::Client::Context.new
-      depth_or_recurse=::Svn::Core::DEPTH_EMPTY
-      begin
-        context.list(@path, "HEAD", nil, depth_or_recurse) do |path, dirent, lock, abs_path|
-          @stat = dirent
-          return @app.call(@env) if @stat.file?
-          return list_directory if @stat.directory?
+        if forbidden = check_forbidden
+          forbidden
+        else
+          @path = F.join(@root, @branch_name, @path_info).sub(/\/$/,'')
+          list_path
         end
-      rescue ::Svn::Error::FsNotFound
-        raise Errno::ENOENT, 'No such file or directory'
-      end
-    rescue Errno::ENOENT, Errno::ELOOP
-      return entity_not_found
-    end
-
-    def entity_not_found
-      body = "Entity not found: #{@path_info}\n"
-      size = Rack::Utils.bytesize(body)
-      return [404, {"Content-Type" => "text/plain",
-        "Content-Length" => size.to_s,
-        "X-Cascade" => "pass"}, [body]]
-    end
-
-    def each
-      show_path = @path.sub(/^#{@root}/,'')
-      files = @files.map{|f| DIR_FILE % f }*"\n"
-      page  = DIR_PAGE % [ show_path, show_path , files ]
-      page.each_line{|l| yield l }
-    end
-
-    # Stolen from Ramaze
-
-    FILESIZE_FORMAT = [
-      ['%.1fT', 1 << 40],
-      ['%.1fG', 1 << 30],
-      ['%.1fM', 1 << 20],
-      ['%.1fK', 1 << 10],
-    ]
-
-    def filesize_format(int)
-      FILESIZE_FORMAT.each do |format, size|
-        return format % (int.to_f / size) if int >= size
       end
 
-      int.to_s + 'B'
+      def check_forbidden
+        return unless @path_info.include? ".."
+
+        body = "Forbidden\n"
+        size = Rack::Utils.bytesize(body)
+        return [403, {"Content-Type" => "text/plain",
+            "Content-Length" => size.to_s,
+            "X-Cascade" => "pass"}, [body]]
+      end
+
+      def list_directory
+        @files = [['../','Parent Directory','','','']]
+
+        context   = ::Svn::Client::Context.new
+
+        context.list(@path ,"HEAD") do |name,dirent,lock,abs_path|
+          basename = F.basename(name)
+          ext = F.extname(name)
+
+          url = F.join(@script_name, @path_info, basename)
+          size = dirent.size
+          type = dirent.directory? ? 'directory' : Mime.mime_type(ext)
+          size = dirent.directory? ? '-' : filesize_format(size)
+          mtime = dirent.time2.httpdate
+          url << '/'  if dirent.directory?
+          basename << '/'  if dirent.directory?
+
+          @files << [ url, basename, size, type, mtime ]
+        end
+        return [ 200, {'Content-Type'=>'text/html; charset=utf-8'}, self ]
+      end
+
+      #def stat(node, max = 10)
+      #  F.stat(node)
+      #rescue Errno::ENOENT, Errno::ELOOP
+      #  return nil
+      #end
+
+      # TODO: add correct response if not readable, not sure if 404 is the best
+      #       option
+      def list_path
+
+        context   = ::Svn::Client::Context.new
+        depth_or_recurse=::Svn::Core::DEPTH_EMPTY
+        begin
+          context.list(@path, "HEAD", nil, depth_or_recurse) do |path, dirent, lock, abs_path|
+            @stat = dirent
+            return @app.call(@env) if @stat.file?
+            return list_directory if @stat.directory?
+          end
+        rescue ::Svn::Error::FsNotFound
+          raise Errno::ENOENT, 'No such file or directory'
+        end
+      rescue Errno::ENOENT, Errno::ELOOP
+        return entity_not_found
+      end
+
+      def entity_not_found
+        body = "Entity not found: #{@path_info}\n"
+        size = Rack::Utils.bytesize(body)
+        return [404, {"Content-Type" => "text/plain",
+            "Content-Length" => size.to_s,
+            "X-Cascade" => "pass"}, [body]]
+      end
+
+      def each
+        show_path = @path.sub(/^#{@root}/,'')
+        files = @files.map{|f| DIR_FILE % f }*"\n"
+        page  = DIR_PAGE % [ show_path, show_path , files ]
+        page.each_line{|l| yield l }
+      end
+
+      # Stolen from Ramaze
+
+      FILESIZE_FORMAT = [
+        ['%.1fT', 1 << 40],
+        ['%.1fG', 1 << 30],
+        ['%.1fM', 1 << 20],
+        ['%.1fK', 1 << 10],
+      ]
+
+      def filesize_format(int)
+        FILESIZE_FORMAT.each do |format, size|
+          return format % (int.to_f / size) if int >= size
+        end
+
+        int.to_s + 'B'
+      end
     end
   end
-end
 end
--- lib/rack/svn/file.rb
+++ lib/rack/svn/file.rb
@@ -5,89 +5,89 @@ require 'svn/client'
 
 module Rack
   module Svn
-class File
-  attr_accessor :root
-  attr_accessor :path
+    class File
+      attr_accessor :root
+      attr_accessor :path
 
-  alias :to_path :path
+      alias :to_path :path
 
-  def initialize(root)
-    @root = root
-  end
+      def initialize(root)
+        @root = root
+      end
 
-  def call(env)
-    dup._call(env)
-  end
+      def call(env)
+        dup._call(env)
+      end
 
-  F = ::File
+      F = ::File
 
-  def _call(env)
-    @branch_name = env['PATH_PREFIX'] || ""
-    @path_info = Rack::Utils.unescape(env["PATH_INFO"])
-    return forbidden  if @path_info.include? ".."
+      def _call(env)
+        @branch_name = env['PATH_PREFIX'] || ""
+        @path_info = Rack::Utils.unescape(env["PATH_INFO"])
+        return forbidden  if @path_info.include? ".."
 
-    @path = F.join(@root, @branch_name, @path_info)
+        @path = F.join(@root, @branch_name, @path_info)
 
-    begin
-      #if F.file?(@path) && F.readable?(@path)
-      serving
-      #else
-      #raise Errno::EPERM
-      #end
-    rescue SystemCallError
-      not_found
-    end
-  end
+        begin
+          #if F.file?(@path) && F.readable?(@path)
+          serving
+          #else
+          #raise Errno::EPERM
+          #end
+        rescue SystemCallError
+          not_found
+        end
+      end
 
-  def forbidden
-    body = "Forbidden\n"
-    [403, {"Content-Type" => "text/plain",
-        "Content-Length" => body.size.to_s,
-        "X-Cascade" => "pass"},
-      [body]]
-  end
+      def forbidden
+        body = "Forbidden\n"
+        [403, {"Content-Type" => "text/plain",
+            "Content-Length" => body.size.to_s,
+            "X-Cascade" => "pass"},
+          [body]]
+      end
 
-  # NOTE:
-  #   We check via File::size? whether this file provides size info
-  #   via stat (e.g. /proc files often don't), otherwise we have to
-  #   figure it out by reading the whole file into memory. And while
-  #   we're at it we also use this as body then.
+      # NOTE:
+      #   We check via File::size? whether this file provides size info
+      #   via stat (e.g. /proc files often don't), otherwise we have to
+      #   figure it out by reading the whole file into memory. And while
+      #   we're at it we also use this as body then.
 
-  def serving
-    context   = ::Svn::Client::Context.new
-    body      = context.cat(@path, "HEAD","HEAD")
+      def serving
+        context   = ::Svn::Client::Context.new
+        body      = context.cat(@path, "HEAD","HEAD")
 
-    last_modified = nil
-    size          = nil
-    context.list(@path ,"HEAD","HEAD") do |name,dirent,lock,abs_path|
-      size          = dirent.size
-      last_modified = dirent.time2
-    end
+        last_modified = nil
+        size          = nil
+        context.list(@path ,"HEAD","HEAD") do |name,dirent,lock,abs_path|
+          size          = dirent.size
+          last_modified = dirent.time2
+        end
 
-    [200, {
-        "Last-Modified"  => last_modified.httpdate,
-        "Content-Type"   => Rack::Mime.mime_type(F.extname(@path_info), 'text/plain'),
-        "Content-Length" => size.to_s
-      }, body]
-  end
+        [200, {
+            "Last-Modified"  => last_modified.httpdate,
+            "Content-Type"   => Rack::Mime.mime_type(F.extname(@path_info), 'text/plain'),
+            "Content-Length" => size.to_s
+          }, body]
+      end
 
-  def not_found
-    body = "File not found: #{@path_info}\n"
-    [404, {"Content-Type" => "text/plain",
-        "Content-Length" => body.size.to_s,
-        "X-Cascade" => "pass"},
-      [body]]
-  end
+      def not_found
+        body = "File not found: #{@path_info}\n"
+        [404, {"Content-Type" => "text/plain",
+            "Content-Length" => body.size.to_s,
+            "X-Cascade" => "pass"},
+          [body]]
+      end
 
-  def each
-    context   = ::Svn::Client::Context.new
-    body      = context.cat(@path, "HEAD","HEAD")
-    StringIO.new(body, "rb") do |file|
-      while part = file.read(8192)
-        yield part
+      def each
+        context   = ::Svn::Client::Context.new
+        body      = context.cat(@path, "HEAD","HEAD")
+        StringIO.new(body, "rb") do |file|
+          while part = file.read(8192)
+            yield part
+          end
+        end
       end
     end
   end
-end
-end
 end
